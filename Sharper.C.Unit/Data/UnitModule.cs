a05611888f4dc30fccf88007ab7ba55083ab878e
Jeremy Hughes <jeremy@tflite.com>
2016-05-23 17:03:48 +1200

    v1.0

--- Sharper.C.Unit/Data/UnitModule.cs
+++ Sharper.C.Unit/Data/UnitModule.cs
@@ -3,42 +3,41 @@ using System;
 namespace Sharper.C.Data
 {
 
-public static class UnitModule
-{
     public sealed class Unit
     {
         private Unit()
         {
         }
+
+        public static Unit UNIT
+        => null;
+
+        public static Action ToAction(Func<Unit> f)
+        =>
+            () => f();
+
+        public static Action<A> ToAction<A>(Func<A, Unit> f)
+        =>
+            a => f(a);
+
+        public static Func<Unit> ToFunc(Action action)
+        =>
+            () =>
+            {
+                action();
+                return UNIT;
+            };
+
+        public static Func<A, Unit> ToFunc<A>(Action<A> action)
+        =>
+            a =>
+            {
+                action(a);
+                return UNIT;
+            };
+
+        public static void Void<A>(A a)
+        {
+        }
     }
-
-    public static Unit UNIT
-    =>
-        default(Unit);
-
-    public static Action ToAction(Func<Unit> f)
-    =>
-        () => f();
-
-    public static Action<A> ToAction<A>(Func<A, Unit> f)
-    =>
-        a => f(a);
-
-    public static Func<Unit> ToFunc(Action action)
-    =>
-        () =>
-        {
-            action();
-            return UNIT;
-        };
-
-    public static Func<A, Unit> ToFunc<A>(Action<A> action)
-    =>
-        a =>
-        {
-            action(a);
-            return UNIT;
-        };
-}
-
 }
